a1fae8f2ffda6d915c546b202d8e793eb79436f3
davomartinez-dev <davidelimg1@gmail.com>
2020-10-16 15:50:29 -0500

    Adding min method

--- minStack/main.rb
+++ minStack/main.rb
@@ -92,17 +92,22 @@ class Stack
 end
 
 stack = Stack.new
+
 stack.push(3)
 stack.push(5)
 puts stack.pop
-# => 5
-stack.push(2)
-stack.push(7)
-puts stack.pop
-# => 7
-
-puts stack.pop
-# => 2
-
-puts stack.pop
+# puts stack.min
 # => 3
+
+# stack.pop
+# stack.push(7)
+# puts stack.min
+# # => 3
+
+# stack.push(2)
+# puts stack.min
+# # => 2
+
+# stack.pop
+# puts stack.min
+# # => 3
