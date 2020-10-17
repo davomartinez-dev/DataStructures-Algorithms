a2baba41274f8ede47dec2fae5634acd5a755e4c
davomartinez-dev <davidelimg1@gmail.com>
2020-10-16 20:56:57 -0500

    finish implementation of slidingMaximum

--- slidingMaximum/main.rb
+++ slidingMaximum/main.rb
@@ -1,8 +1,25 @@
 require 'byebug'
 
 def sliding_maximum(k, array)
-  # your code here
+  return -1 if k > array.size
+
+  result = []
+  window_max = 0
+  it = 0
+
+  while it < array.length
+    window_max = array[it] if array[it] > window_max
+
+    if it >= k - 1
+      result.push(window_max)
+      window_max = array[it]
+    end
+
+    it += 1
+  end
+  result
 end
 
-sliding_maximum(3, [1, 3, 5, 7, 9, 2])
-# => [5, 7, 9, 9]
+# print sliding_maximum(3, [1, 3, 5, 7, 9, 2]) # [5, 7, 9, 9]
+# print sliding_maximum(4, [9, 3, 5, 1, 7, 10]) # [9, 7, 10]
+print sliding_maximum(3, [1, 3, 5, 7, 9, 2]) # [5, 7, 9, 9]
