bffe01e400c0d802357fb6685867b61518c3983c
davomartinez-dev <davidelimg1@gmail.com>
2020-10-20 17:12:39 -0500

    Transposition

---
+++ transposition/main.rb
@@ -0,0 +1,14 @@
+require 'byebug'
+
+def transpose(_string)
+  byebug
+end
+
+puts transpose('he was searchign on Bign for signign kigns')
+# => he was searching on Bing for singing kings
+
+# puts transpose('rignadingdiggn!')
+# # => ringadingdingg!
+
+# puts transpose('gngngnnggnngggnnn')
+# # => nnnnnnnnngggggggg
